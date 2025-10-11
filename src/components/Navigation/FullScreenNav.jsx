dcdb772df771c529a762367687e2aeface29ec31
jaack01 <62318308+jaack01@users.noreply.github.com>
2025-10-12 01:22:43 +0530

    Updated index.css

--- src/components/Navigation/FullScreenNav.jsx
+++ src/components/Navigation/FullScreenNav.jsx
@@ -84,27 +84,24 @@ const FullScreenNav = () => {
                 </div>
             </div>
             <div ref={fullNavLinksRef} className='relative h-screen overflow-y-auto'>
-                <div className="navlink flex w-full justify-between lg:p-6 p-3 items-start">
-                    <Link to='/' onClick={() => setNavOpen(false)} className='transition-opacity duration-300 hover:opacity-70'>
-                        <div className='lg:w-[140px] w-[90px]'>
-                            <svg className='w-full' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 103 44">
+                <div className="navlink flex w-full justify-between lg:p-5 p-2 items-start">
+                    <div className=''>
+                        <div className='lg:w-36 w-24'>
+                            <svg className=' w-full' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 103 44">
                                 <path fill='white' fillRule="evenodd" d="M35.1441047,8.4486911 L58.6905011,8.4486911 L58.6905011,-1.3094819e-14 L35.1441047,-1.3094819e-14 L35.1441047,8.4486911 Z M20.0019577,0.000230366492 L8.83414254,25.3433089 L18.4876971,25.3433089 L29.5733875,0.000230366492 L20.0019577,0.000230366492 Z M72.5255345,0.000691099476 L72.5255345,8.44846073 L94.3991559,8.44846073 L94.3991559,16.8932356 L72.5275991,16.8932356 L72.5275991,19.5237906 L72.5255345,19.5237906 L72.5255345,43.9274346 L102.80937,43.9274346 L102.80937,35.4798953 L80.9357483,35.4798953 L80.9357483,25.3437696 L94.3996147,25.3428482 L94.3996147,16.8953089 L102.80937,16.8953089 L102.80937,0.000691099476 L72.5255345,0.000691099476 Z M-1.30398043e-14,43.9278953 L8.78642762,43.9278953 L8.78642762,0.0057591623 L-1.30398043e-14,0.0057591623 L-1.30398043e-14,43.9278953 Z M58.6849955,8.4486911 L43.1186904,43.9274346 L52.3166592,43.9274346 L67.9877996,8.4486911 L58.6849955,8.4486911 Z M18.4688864,25.3437696 L26.7045278,43.9278953 L36.2761871,43.9278953 L28.1676325,25.3375497 L18.4688864,25.3437696 Z"></path>
                             </svg>
                         </div>
-                    </Link>
-                    <button
-                        onClick={() => {
-                            setNavOpen(false)
-                        }}
-                        className='lg:h-20 h-16 w-16 lg:w-20 relative cursor-pointer group transition-transform duration-300 hover:scale-110'
-                        aria-label='Close menu'
-                    >
-                        <div className='lg:h-28 h-20 lg:w-[3px] w-[2px] -rotate-45 origin-top absolute bg-[#D3FD50] transition-all duration-300 group-hover:bg-white'></div>
-                        <div className='lg:h-28 h-20 lg:w-[3px] w-[2px] right-0 rotate-45 origin-top absolute bg-[#D3FD50] transition-all duration-300 group-hover:bg-white'></div>
-                    </button>
+                    </div>
+                    <div onClick={() => {
+                        setNavOpen(false)
+                    }} className='lg:h-20 h-16 w-16 lg:w-20 relative cursor-pointer'>
+                        <div className='lg:h-28 h-20 lg:w-1 w-0.5 -rotate-45 origin-top absolute bg-[#D3FD50]'></div>
+                        <div className='lg:h-28 h-20 lg:w-1 w-0.5 right-0 rotate-45 origin-top absolute bg-[#D3FD50]'></div>
+
+                    </div>
                 </div>
                 <div className='lg:py-12 py-8'>
-                    <Link to='/projects' onClick={() => setNavOpen(false)} className='link origin-top relative border-t-1 border-white block cursor-pointer overflow-hidden'>
+                    <Link to='/projects' onClick={() => setNavOpen(false)} className='link origin-top relative border-t-1 border-white block cursor-pointer'>
                         <h1 className='font-[font2] text-4xl lg:text-[6vw] text-center lg:leading-[0.8] lg:py-6 py-2 uppercase'>Projets</h1>
                         <div className='moveLink absolute text-black flex top-0 bg-[#D3FD50]'>
                             <div className='moveX flex items-center'>
@@ -122,7 +119,7 @@ const FullScreenNav = () => {
                         </div>
 
                     </Link>
-                    <Link to='/agence' onClick={() => setNavOpen(false)} className='link origin-top relative border-t-1 border-white block cursor-pointer overflow-hidden'>
+                    <Link to='/agence' onClick={() => setNavOpen(false)} className='link origin-top relative border-t-1 border-white block cursor-pointer'>
                         <h1 className='font-[font2] text-4xl lg:text-[6vw] text-center lg:leading-[0.8] lg:py-6 py-2 uppercase'>Agence</h1>
                         <div className='moveLink absolute text-black flex top-0 bg-[#D3FD50]'>
                             <div className='moveX flex items-center'>
@@ -140,7 +137,7 @@ const FullScreenNav = () => {
                         </div>
 
                     </Link>
-                    <Link to='/contact' onClick={() => setNavOpen(false)} className='link origin-top relative border-t-1 border-white block cursor-pointer overflow-hidden'>
+                    <Link to='/contact' onClick={() => setNavOpen(false)} className='link origin-top relative border-t-1 border-white block cursor-pointer'>
                         <h1 className='font-[font2] text-4xl lg:text-[6vw] text-center lg:leading-[0.8] lg:py-6 py-2 uppercase'>Contact</h1>
                         <div className='moveLink absolute text-black flex top-0 bg-[#D3FD50]'>
                             <div className='moveX flex items-center'>
@@ -158,7 +155,7 @@ const FullScreenNav = () => {
                         </div>
 
                     </Link>
-                    <Link to='/blogs' onClick={() => setNavOpen(false)} className='link origin-top relative border-y-1 border-white block cursor-pointer overflow-hidden'>
+                    <Link to='/blogs' onClick={() => setNavOpen(false)} className='link origin-top relative border-y-1 border-white block cursor-pointer'>
                         <h1 className='font-[font2] text-4xl lg:text-[6vw] text-center lg:leading-[0.8] lg:py-6 py-2 uppercase'>Blogs</h1>
                         <div className='moveLink absolute text-black flex top-0 bg-[#D3FD50]'>
                             <div className='moveX flex items-center'>
